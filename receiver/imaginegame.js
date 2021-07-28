4104c778dafd8f1682bc796b7f5d28a053473630
NichoZeGeek <nicolas.lapeyre@gmail.com>
2021-07-28 23:42:41 +0200

    Prise en compte de la résolution du phone via message Display

--- receiver/imaginegame.js
+++ receiver/imaginegame.js
@@ -2,6 +2,12 @@ var mycontainer;
 var mycanvas;
 var mycontext;
 var myImages = {};
+var phone_gameAreaSize = [1024,760];
+var cast_gameAreaSize = [1024,760];
+var img_phoneSize = 100;
+var ratio_img = img_phoneSize/512;
+var ratio_x = cast_gameAreaSize[0]/phone_gameAreaSize[0];
+var ratio_y = cast_gameAreaSize[1]/phone_gameAreaSize[1];
 const numImages = 31;
 
 var datestart= Date.now();
@@ -13,11 +19,33 @@ var origin = 1 ; // origine au centre du canevas pour les translations
 
 
 function newImagineBoard() {
+    // cast_gameAreaSize = [phone_gameAreaSize[0],phone_gameAreaSize[1]];
+    if (document.body)
+        {
+        // var larg = (document.body.clientWidth);
+        var larg = screen.availWidth;
+        // var haut = (document.body.clientHeight);
+        var haut = screen.availHeight;
+        // console.log("body size largeur = ",larg," et hauteur = ",haut);
+        cast_gameAreaSize = [haut, Math.round(phone_gameAreaSize[1]*haut/phone_gameAreaSize[0])];
+        }
+    console.log("cast_gameAreaSize = ",cast_gameAreaSize);
+    ratio_x = cast_gameAreaSize[0]/phone_gameAreaSize[0];
+    ratio_y = cast_gameAreaSize[1]/phone_gameAreaSize[1];
+    console.log("ratio_x = ",ratio_x);
+    console.log("ratio_y = ",ratio_y);
+
     document.getElementById('imagineBoard').remove();
+    document.getElementById('imagineBoard-container').style.width = cast_gameAreaSize[1]+"px";
+    document.getElementById('imagineBoard-container').style.height = cast_gameAreaSize[0]+"px";
     document.getElementById('imagineBoard-container').innerHTML = "<canvas id='imagineBoard' width='1000' height='1000' style='width:100%;height: 100%;'></canvas>";
+    // document.getElementById('imagineBoard-container').innerHTML = "<canvas id='imagineBoard' width='100' height='100'></canvas>";
+    // document.getElementById('imagineBoard-container').innerHTML = "<canvas id='imagineBoard' width=phone_gameAreaSize[1] height=phone_gameAreaSize[0] style='width:100%;height: 100%;'></canvas>";
     // document.getElementById('imagineBoard-container').innerHTML = "<div id='toto' style = 'position:absolute;'><img src='img/0.svg' /></div>";
     // window.checkDebug();
     mycanvas = document.getElementById('imagineBoard');
+    mycanvas.width=phone_gameAreaSize[1];
+    mycanvas.height=phone_gameAreaSize[0];
     mycontext = mycanvas.getContext('2d');
     mycontext.font = "bold 20px sans-serif";
     // mycontainer = document.getElementById('imagineBoard-container');
@@ -28,6 +56,8 @@ function newImagineBoard() {
     for (var i=0; i<numImages; i++) { 
         myImages[i] = new Image();
         myImages[i].src = 'img/'+i+'.png';
+        // myImages[i].width = '50px';
+        // myImages[i].height = '50px';
         myImages[i].id = i;
         // myImages[i].onload = function() {
         //     if(++loadedImages >= numImages) {
@@ -73,7 +103,9 @@ function generateMessageNicho(data){
         //   var intervalID = window.setInterval(handleMessage, 20, 'I|16:0:0:100:100:0:1:1|25:375:375:20:20:45:1:1|13:375:175:50:50:75:-1:1', 'Nicho_Id');
 
         //   var timerintervalID = window.setInterval(handleMessage, 1, 'T|05:49', 'Nicho_Id');
-          handleMessage('I|13:0:0:100:100:0:1:1|25:375:375:20:20:45:1:1|13:375:175:50:50:75:-1:1', 'Nicho_Id');
+        handleMessage('D|5300:3600', 'Nicho_Id');
+        handleMessage('T|05:49', 'Nicho_Id');
+        handleMessage('I|1:0:0:200:200:0:1:1|2:90:132:100:100:60:1:1|3:-90:-100:50:50:-75:-1:1', 'Nicho_Id');
         //   handleMessage('I|18:275:375:100:100:-30:1:1|24:375:375:100:100:45:1:1','Nicho_Id')
         //   if (leftGlobal<300)
           {
@@ -99,6 +131,7 @@ function generateMessageNicho(data){
 function handleMessage(myData, mySenderId) {
 
     // var tableau_parametres_images = myData.split("|");
+    console.log("myData[0]",myData[0]);
     switch (myData[0]) {
         case 'I' :
             displayImages(myData, mySenderId);
@@ -106,11 +139,26 @@ function handleMessage(myData, mySenderId) {
         case 'T' :
             displayTimer(myData, mySenderId);
             break;
+        case 'D' :
+            updateGameAreaSize(myData, mySenderId);
+            break;
         default :
             console.log('Undefined message received '+myData);
     }
     
 };
+
+
+function updateGameAreaSize(myData, mySenderId) {
+    var tableau_gameAreaSize = myData.split("|");
+
+    phone_gameAreaSize = tableau_gameAreaSize[1].split(":");
+    phone_gameAreaSize[0] = 0.1*phone_gameAreaSize[0];
+    phone_gameAreaSize[1] = 0.1*phone_gameAreaSize[1];
+    console.log("phone_gameAreaSize = ",phone_gameAreaSize);
+    newImagineBoard();
+    
+}
 
 function displayTimer(myData, mySenderId) {
     var tableau_timer = myData.split("|");
@@ -145,18 +193,22 @@ function displayImages(myData, mySenderId) {
             console.log('mycanvas.width',mycanvas.width)
             console.log('mycanvas.height',mycanvas.height)
             
-            console.log('current_image_parameters[2] + mycanvas.height/2 = ',1*current_image_parameters[2] + 1*mycanvas.height/2)
-            if (origin==0)
+           if (origin==0)
             {
-                mycontext.translate((1*current_image_parameters[1]+1*buibuiGlobal)%mycanvas.width, current_image_parameters[2]); 
+                // mycontext.translate((1*current_image_parameters[1]+1*buibuiGlobal)%mycanvas.width, current_image_parameters[2]); 
+                mycontext.translate((1*current_image_parameters[1]), (1*current_image_parameters[2])); 
             }
             else if (origin==1) 
             {
-                mycontext.translate((1*current_image_parameters[1]+1*buibuiGlobal)%mycanvas.width + 1*mycanvas.width/2, 1*current_image_parameters[2] + 1*mycanvas.height/2);
+                // mycontext.translate((1*current_image_parameters[1]+1*buibuiGlobal)%mycanvas.width + 1*mycanvas.width/2, 1*current_image_parameters[2] + 1*mycanvas.height/2);
+                // console.log("translate canvas",(1*current_image_parameters[1]) + 1*mycanvas.width/2, (1*current_image_parameters[2]) + 1*mycanvas.height/2);
+                mycontext.translate((1*current_image_parameters[1]) + 1*mycanvas.width/2, (1*current_image_parameters[2]) + 1*mycanvas.height/2);
             }
-            mycontext.rotate(((buibuiGlobal)*1+(current_image_parameters[5])*1)*(2*Math.PI)/360);
+            // mycontext.rotate(((buibuiGlobal)*1+(current_image_parameters[5])*1)*(2*Math.PI)/360);
+            mycontext.rotate(((current_image_parameters[5])*1)*(2*Math.PI)/360);
             mycontext.scale(current_image_parameters[6]*current_image_parameters[3]/100,current_image_parameters[7]*current_image_parameters[4]/100);
-            mycontext.drawImage(myImages[current_image_id], -myImages[current_image_id].width/2, -myImages[current_image_id].height/2);
+            mycontext.drawImage(myImages[current_image_id], -myImages[current_image_id].width*ratio_img/2, -myImages[current_image_id].height*ratio_img/2, img_phoneSize,img_phoneSize);
+            // mycontext.drawImage(myImages[current_image_id], -myImages[current_image_id].width/2, -myImages[current_image_id].height/2);
             mycontext.restore(); 
     }   
 
